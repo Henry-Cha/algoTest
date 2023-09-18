00975a34704fe59e137d24dd88b6af3e704ab5a0
Cha Sung won <74866067+Henry-Cha@users.noreply.github.com>
2023-09-18 20:49:00 +0900

    23/09/18 차성원 G3 BOJ16236
    (이전 커밋 1038임)

---
+++ 차성원/BOJ1038.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BOJ1038 {
+    static List<Long> list;
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        list= new ArrayList<>();
+        if(N<=10) {
+            System.out.println(N);
+            return;
+        }else if(N>1022) {
+            System.out.println(-1);
+            return;
+        }
+        for(int i=0;i<10;i++){
+            find(i);
+        }
+        list.sort(null);
+        System.out.println(list.get(N));
+    }
+    public static void find(long i){
+        list.add(i);
+        long next = i%10;
+        if(next==0) return;
+        for(long j=next-1;j>-1;j--){
+            find(i*10+j);
+        }
+    }
+}
--- 차성원/BOJ16236.java
+++ 차성원/BOJ16236.java
@@ -1,35 +1,88 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class BOJ16236 {
-    static List<Long> list;
+    static int[][] map;
     static int N;
+    static Point shark;
+    static int sharkSize = 2;
+    static int sizeUpCnt=0;
+    static int time = 0;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static boolean[][] visit;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        list= new ArrayList<>();
-        if(N<=10) {
-            System.out.println(N);
-            return;
-        }else if(N>1022) {
-            System.out.println(-1);
-            return;
+        map = new int[N][N];
+        StringTokenizer st = null;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 9) {
+                    shark = new Point(i, j,0);
+                    map[i][j]=0;
+                }
+            }
         }
-        for(int i=0;i<10;i++){
-            find(i);
+        while(true) {
+            visit = new boolean[N][N];
+            PriorityQueue<Point> q = new PriorityQueue<>();
+            q.add(shark);
+            visit[shark.x][shark.y] = true;
+            boolean flag = false;
+            while (!q.isEmpty()) {
+                Point now = q.poll();
+                if(map[now.x][now.y]>0 && map[now.x][now.y]<sharkSize){
+                    map[now.x][now.y]=0;
+                    shark=new Point(now.x,now.y,0);
+                    sizeUpCnt++;
+                    time += (now.dis);
+                    flag=true;
+                    if(sizeUpCnt==sharkSize){
+                        sharkSize++;
+                        sizeUpCnt=0;
+                    }
+                    break;
+                }
+                for (int d = 0; d < 4; d++) {
+                    int nx = now.x + dx[d];
+                    int ny = now.y + dy[d];
+                    if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+                    if(visit[nx][ny]) continue;
+                    if (map[nx][ny] > sharkSize) continue;
+                    q.add(new Point(nx,ny, now.dis+1));
+                    visit[nx][ny] = true;
+                }
+            }
+            if(!flag) break;
         }
-        list.sort(null);
-        System.out.println(list.get(N));
+        System.out.println(time);
     }
-    public static void find(long i){
-        list.add(i);
-        long next = i%10;
-        if(next==0) return;
-        for(long j=next-1;j>-1;j--){
-            find(i*10+j);
+    static class Point implements Comparable<Point> {
+        int x;
+        int y;
+        int dis;
+
+        public Point(int x, int y,int dis) {
+            this.x = x;
+            this.y = y;
+            this.dis=dis;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            if(this.dis == o.dis) {
+                if (this.x == o.x) {
+                    return this.y - o.y;
+                }
+                return this.x - o.x;
+            }return this.dis-o.dis;
         }
     }
 }
